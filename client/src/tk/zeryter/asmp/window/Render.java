3bb0f86d3bb55fb66c8c83213033ae184d946abd
Owen Holloway <owen.holloway101@gmail.com>
2013-08-14 18:30:54 +1000

    Things and stuff

--- client/src/tk/zeryter/asmp/window/Render.java
+++ client/src/tk/zeryter/asmp/window/Render.java
@@ -9,6 +9,8 @@ public class Render implements Runnable {
 
     DisplayMode window;
 
+    int size = 10;
+
     public void run() {
 
         Shapes s = new Shapes();
@@ -33,15 +35,37 @@ public class Render implements Runnable {
         GL11.glOrtho(0, Display.getWidth(), 0, Display.getHeight(), 1, -1);    //This sets up the render space for open GL pixW is width pixH is Height
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
+        int sizeAdd = 5;
+
+        int asdf = 0;
+
         while (!Display.isCloseRequested()) {
 
             GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);      //Clears the render space
 
             //Draw to the render space
 
-            rectangle(350, 250, 20, 20);
+            s.rectangle(350, 300, 20 + size, 20 + size,1,0,0,80);
+            s.rectangle(350, 200, 20 + size, 20 + size,0,1,0,80);
+            s.rectangle(350, 250, 20 + size, 20 + size,0,0,1,80);
+
+            s.rectangle(500,300,20 + size,20 + size,0,0,1,80);
+            s.rectangle(500,200,20 + size,20 + size,1,0,0,80);
+            s.rectangle(500,250,20 + size,20 + size,0,1,0,80);
+
+            s.rectangle(200,300,20 + size,20 + size,0,1,0,80);
+            s.rectangle(200,200,20 + size,20 + size,0,0,1,80);
+            s.rectangle(200,250,20 + size,20 + size,1,0,0,80);
+
+            //s.triangle(350,250,5,10,5,Math.PI/4);
 
             Display.update();
+
+                if (size >= 400 | size <= 0) sizeAdd = sizeAdd * -1;
+
+                size = size + sizeAdd;
+
+            //System.out.println("size: " + size);
 
             try {
                 Thread.sleep(16);
@@ -51,16 +75,5 @@ public class Render implements Runnable {
 
 
         }
-    }
-
-    public void rectangle(int x, int y, int width, int height){
-
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2i(x - width/2,y + height/2);
-        GL11.glVertex2i(x + width/2,y + height/2);
-        GL11.glVertex2i(x + width/2,y - height/2);
-        GL11.glVertex2i(x - width/2,y - height/2);
-        GL11.glEnd();
-
     }
 }
--- client/src/tk/zeryter/asmp/window/Shapes.java
+++ client/src/tk/zeryter/asmp/window/Shapes.java
@@ -4,15 +4,24 @@ import org.lwjgl.opengl.GL11;
 
 public class Shapes {
 
-    int size = 1;
-    int sizeAdd = 10;
+    public void triangle(int x, int y, double v1, double v2, double v3, double rotation,double R, double G, double B) {
 
-    public void triangle(int x, int y, double rotation) {
+        int[] xPos = {x + (int) (v1 * Math.cos(rotation + (5 * Math.PI) / 4)), x + (int) (v2 * Math.cos(rotation)), x + (int) (v3 * Math.cos(rotation + (3 * Math.PI) / 4))};
+        int[] yPos = {y + (int) (v1 * Math.sin(rotation + (5 * Math.PI) / 4)), y + (int) (v2 * Math.sin(rotation)), y + (int) (v3 * Math.sin(rotation + (3 * Math.PI) / 4))};
 
-        int[] xPos = {x + (int) (10 * Math.sqrt(5) * Math.cos(rotation + (5 * Math.PI) / 4)), x + (int) (20 * Math.cos(rotation)), x + (int) (10 * Math.sqrt(5) * Math.cos(rotation + (3 * Math.PI) / 4))};
-        int[] yPos = {y + (int) (10 * Math.sqrt(5) * Math.sin(rotation + (5 * Math.PI) / 4)), y + (int) (20 * Math.sin(rotation)), y + (int) (10 * Math.sqrt(5) * Math.sin(rotation + (3 * Math.PI) / 4))};
+        GL11.glColor3d(R, G, B);
 
-        GL11.glColor3d(0, 1, 0);
+        GL11.glBegin(GL11.GL_TRIANGLES);
+            GL11.glVertex2i(xPos[0], yPos[0]);
+            GL11.glVertex2i(xPos[1], yPos[1]);
+            GL11.glVertex2i(xPos[2], yPos[2]);
+        GL11.glEnd();
+    }
+
+    public void triangle(int x, int y,double v1, double v2, double v3, double rotation) {
+
+        int[] xPos = {x + (int) (v1 * Math.cos(rotation + (5 * Math.PI) / 4)), x + (int) (v2 * Math.cos(rotation)), x + (int) (v3 * Math.cos(rotation + (3 * Math.PI) / 4))};
+        int[] yPos = {y + (int) (v1 * Math.sin(rotation + (5 * Math.PI) / 4)), y + (int) (v2 * Math.sin(rotation)), y + (int) (v3 * Math.sin(rotation + (3 * Math.PI) / 4))};
 
         GL11.glBegin(GL11.GL_TRIANGLES);
             GL11.glVertex2i(xPos[0], yPos[0]);
@@ -28,6 +37,32 @@ public class Shapes {
             GL11.glVertex2i(x + width/2,y + height/2);
             GL11.glVertex2i(x + width/2,y - height/2);
             GL11.glVertex2i(x - width/2,y - height/2);
+        GL11.glEnd();
+
+    }
+
+    public void rectangle(int x, int y, int width, int height,double R,double G,double B){
+
+        GL11.glColor3d(R,G,B);
+
+        GL11.glBegin(GL11.GL_QUADS);
+            GL11.glVertex2i(x - width/2,y + height/2);
+            GL11.glVertex2i(x + width/2,y + height/2);
+            GL11.glVertex2i(x + width/2,y - height/2);
+            GL11.glVertex2i(x - width/2,y - height/2);
+        GL11.glEnd();
+
+    }
+
+    public void rectangle(int x, int y, int width, int height,double R,double G,double B, double A){
+
+        GL11.glColor4d(R,G,B,0);
+
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2i(x - width/2,y + height/2);
+        GL11.glVertex2i(x + width/2,y + height/2);
+        GL11.glVertex2i(x + width/2,y - height/2);
+        GL11.glVertex2i(x - width/2,y - height/2);
         GL11.glEnd();
 
     }
