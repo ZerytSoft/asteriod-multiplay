9664f713704808fcdbd989dc94fa1ba7abd1eb37
Owen Holloway <owen.holloway101@gmail.com>
2013-08-15 20:55:11 +1000

    Tiding up the code

--- client/src/tk/zeryter/asmp/window/Shapes.java
+++ client/src/tk/zeryter/asmp/window/Shapes.java
@@ -1,6 +1,6 @@
 package tk.zeryter.asmp.window;
 
-import org.lwjgl.opengl.GL11;
+import static org.lwjgl.opengl.GL11.*;
 
 public class Shapes {
 
@@ -9,13 +9,13 @@ public class Shapes {
         int[] xPos = {x + (int) (v1 * Math.cos(rotation + (5 * Math.PI) / 4)), x + (int) (v2 * Math.cos(rotation)), x + (int) (v3 * Math.cos(rotation + (3 * Math.PI) / 4))};
         int[] yPos = {y + (int) (v1 * Math.sin(rotation + (5 * Math.PI) / 4)), y + (int) (v2 * Math.sin(rotation)), y + (int) (v3 * Math.sin(rotation + (3 * Math.PI) / 4))};
 
-        GL11.glColor3d(R, G, B);
+        glColor3d(R, G, B);
 
-        GL11.glBegin(GL11.GL_TRIANGLES);
-            GL11.glVertex2i(xPos[0], yPos[0]);
-            GL11.glVertex2i(xPos[1], yPos[1]);
-            GL11.glVertex2i(xPos[2], yPos[2]);
-        GL11.glEnd();
+        glBegin(GL_TRIANGLES);
+            glVertex2i(xPos[0], yPos[0]);
+            glVertex2i(xPos[1], yPos[1]);
+            glVertex2i(xPos[2], yPos[2]);
+        glEnd();
     }
 
     public void triangle(int x, int y,double v1, double v2, double v3, double rotation) {
@@ -23,11 +23,11 @@ public class Shapes {
         int[] xPos = {x + (int) (v1 * Math.cos(rotation + (5 * Math.PI) / 4)), x + (int) (v2 * Math.cos(rotation)), x + (int) (v3 * Math.cos(rotation + (3 * Math.PI) / 4))};
         int[] yPos = {y + (int) (v1 * Math.sin(rotation + (5 * Math.PI) / 4)), y + (int) (v2 * Math.sin(rotation)), y + (int) (v3 * Math.sin(rotation + (3 * Math.PI) / 4))};
 
-        GL11.glBegin(GL11.GL_TRIANGLES);
-            GL11.glVertex2i(xPos[0], yPos[0]);
-            GL11.glVertex2i(xPos[1], yPos[1]);
-            GL11.glVertex2i(xPos[2], yPos[2]);
-        GL11.glEnd();
+        glBegin(GL_TRIANGLES);
+            glVertex2i(xPos[0], yPos[0]);
+            glVertex2i(xPos[1], yPos[1]);
+            glVertex2i(xPos[2], yPos[2]);
+        glEnd();
     }
 
     public void rectangle(int x, int y, int width, int height, double rotation){
@@ -38,14 +38,14 @@ public class Shapes {
 
         double angle = Math.atan((-height/2)/(-width/2));
 
-        GL11.glColor3d(1, 1, 1);
+        glColor3d(1, 1, 1);
 
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2i((int)(x + ((hyp)*Math.cos(angle + rotation))),(int)(y + ((hyp)*Math.sin(angle + rotation))));
-        GL11.glVertex2i((int)(x - ((hyp)*Math.cos(angle - rotation))),(int)(y + ((hyp)*Math.sin(angle - rotation))));
-        GL11.glVertex2i((int)(x - ((hyp)*Math.cos(angle + rotation))),(int)(y - ((hyp)*Math.sin(angle + rotation))));
-        GL11.glVertex2i((int)(x + ((hyp)*Math.cos(angle - rotation))),(int)(y - ((hyp)*Math.sin(angle - rotation))));
-        GL11.glEnd();;
+        glBegin(GL_QUADS);
+            glVertex2i((int) (x + ((hyp) * Math.cos(angle + rotation))), (int) (y + ((hyp) * Math.sin(angle + rotation))));
+            glVertex2i((int) (x - ((hyp) * Math.cos(angle - rotation))), (int) (y + ((hyp) * Math.sin(angle - rotation))));
+            glVertex2i((int) (x - ((hyp) * Math.cos(angle + rotation))), (int) (y - ((hyp) * Math.sin(angle + rotation))));
+            glVertex2i((int) (x + ((hyp) * Math.cos(angle - rotation))), (int) (y - ((hyp) * Math.sin(angle - rotation))));
+        glEnd();;
 
     }
 
@@ -57,14 +57,14 @@ public class Shapes {
 
         double angle = Math.atan((-height/2)/(-width/2));
 
-        GL11.glColor3d(R, G, B);
+        glColor3d(R, G, B);
 
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2i((int)(x + ((hyp)*Math.cos(angle + rotation))),(int)(y + ((hyp)*Math.sin(angle + rotation))));
-        GL11.glVertex2i((int)(x - ((hyp)*Math.cos(angle - rotation))),(int)(y + ((hyp)*Math.sin(angle - rotation))));
-        GL11.glVertex2i((int)(x - ((hyp)*Math.cos(angle + rotation))),(int)(y - ((hyp)*Math.sin(angle + rotation))));
-        GL11.glVertex2i((int)(x + ((hyp)*Math.cos(angle - rotation))),(int)(y - ((hyp)*Math.sin(angle - rotation))));
-        GL11.glEnd();
+        glBegin(GL_QUADS);
+            glVertex2i((int) (x + ((hyp) * Math.cos(angle + rotation))), (int) (y + ((hyp) * Math.sin(angle + rotation))));
+            glVertex2i((int) (x - ((hyp) * Math.cos(angle - rotation))), (int) (y + ((hyp) * Math.sin(angle - rotation))));
+            glVertex2i((int) (x - ((hyp) * Math.cos(angle + rotation))), (int) (y - ((hyp) * Math.sin(angle + rotation))));
+            glVertex2i((int) (x + ((hyp) * Math.cos(angle - rotation))), (int) (y - ((hyp) * Math.sin(angle - rotation))));
+        glEnd();
 
     }
 
@@ -76,14 +76,14 @@ public class Shapes {
 
         double angle = Math.atan((-height/2)/(-width/2));
 
-        GL11.glColor4d(R, G, B, A);
+        glColor4d(R, G, B, A);
 
-        GL11.glBegin(GL11.GL_QUADS);
-        GL11.glVertex2i((int)(x + ((hyp)*Math.cos(angle + rotation))),(int)(y + ((hyp)*Math.sin(angle + rotation))));
-        GL11.glVertex2i((int)(x - ((hyp)*Math.cos(angle - rotation))),(int)(y + ((hyp)*Math.sin(angle - rotation))));
-        GL11.glVertex2i((int)(x - ((hyp)*Math.cos(angle + rotation))),(int)(y - ((hyp)*Math.sin(angle + rotation))));
-        GL11.glVertex2i((int)(x + ((hyp)*Math.cos(angle - rotation))),(int)(y - ((hyp)*Math.sin(angle - rotation))));
-        GL11.glEnd();
+        glBegin(GL_QUADS);
+            glVertex2i((int) (x + ((hyp) * Math.cos(angle + rotation))), (int) (y + ((hyp) * Math.sin(angle + rotation))));
+            glVertex2i((int) (x - ((hyp) * Math.cos(angle - rotation))), (int) (y + ((hyp) * Math.sin(angle - rotation))));
+            glVertex2i((int) (x - ((hyp) * Math.cos(angle + rotation))), (int) (y - ((hyp) * Math.sin(angle + rotation))));
+            glVertex2i((int) (x + ((hyp) * Math.cos(angle - rotation))), (int) (y - ((hyp) * Math.sin(angle - rotation))));
+        glEnd();
 
     }
 }
